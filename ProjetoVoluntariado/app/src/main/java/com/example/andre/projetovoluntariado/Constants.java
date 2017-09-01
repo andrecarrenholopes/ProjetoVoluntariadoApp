d90a44d2a93bd2312659901cfcb87accca151991
andrecarrenholopes <andrecarrenholopes@gmail.com>
2017-09-01 20:49:45 -0300

    configuraçoes iniciais
    registro e login implementados

---
+++ ProjetoVoluntariado/app/src/main/java/com/example/andre/projetovoluntariado/Constants.java
@@ -0,0 +1,11 @@
+package com.example.andre.projetovoluntariado;
+
+/**
+ * Created by Andre on 31/08/2017.
+ */
+
+public class Constants {
+        private static final String ROOT_URL = "http://10.0.3.2/projetovoluntariado/Android/v1/";
+        public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+        public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
+}
